cfb7b2d4b287592ed4fb2335ec3bc189e7d7af82
Mike Axtman <mdaxtman@gmail.com>
2015-04-23 23:46:57 -0700

    update styling, add polyfill to framework for ie compatibility

--- client/scripts/kgb.js
+++ client/scripts/kgb.js
@@ -1,3 +1,60 @@
+/*ChildNode polyfill*/
+(function() {
+  var buildDOM = function() {
+    var nodes = Array.prototype.slice.call(arguments),
+      frag = document.createDocumentFragment(),
+      div, node;
+
+    while(node = nodes.shift()){
+      if(typeof node == "string") {
+        div = document.createElement("div");
+        div.innerHTML = node;
+        while (div.firstChild){
+          frag.appendChild(div.firstChild);
+        }
+      }else{
+        frag.appendChild(node);
+      }
+    }
+    return frag;
+  };
+
+  var proto = {
+    before: function(){
+      var frag = buildDOM.apply(this, arguments);
+      this.parentNode.insertBefore(frag, this);
+    },
+    after: function(){
+      var frag = buildDOM.apply(this, arguments);
+      this.parentNode.insertBefore(frag, this.nextSibling);
+    },
+    replaceWith: function(){
+      if(this.parentNode){
+        var frag = buildDOM.apply(this, arguments);
+        this.parentNode.replaceChild(frag, this);
+      }
+    },
+    remove:function(){
+      if(this.parentNode){
+        this.parentNode.removeChild(this);
+      }
+    }
+  };
+
+  var a = ["Element", "DocumentType", "CharacterData"]; // interface
+  var b = ["before", "after", "replaceWith", "remove"]; // methods
+  a.forEach(function(v){
+    b.forEach(function(func) {
+      if(window[v]){
+        if(window[v].prototype[func]) { return; }
+        window[v].prototype[func] = proto[func];
+      }
+    });
+  });
+})();
+
+
+
 var KGB = function(){
   var body;
   var w = window;
